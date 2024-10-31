d06539b437d72be706d7068b8c7a11189f51565f
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-10-31 16:28:28 +0500

    router file fixed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import userRouter from "./user.route";
 
 let router: Router = Router()
-router.get("/", () => { "Salom" })
+router.get("/", (req: Request, res: Response) => { res.status(200).send({ message: "Assalomu aleykum" }) })
 router.use("/users", userRouter)
 
 export default router
