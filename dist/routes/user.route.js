4c37ba2e9a97ad1d75d7a9cebeb7ca6914011077
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-10-31 15:19:28 +0500

    vercel.json file add

---
+++ dist/routes/user.route.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const controllers_1 = require("src/controllers");
+const userRouter = (0, express_1.Router)();
+userRouter.get("/all", controllers_1.UserController.allUsersData);
+exports.default = userRouter;
